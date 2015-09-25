f06de2da321a9a19317e1f6912154c2675e0ce22
Tom de Vries <tom@hackerone.com>
2015-09-25 11:36:36 +0200

    Support sidekiq.yml parsing

--- lib/sidekiq/health/queue_names.rb
+++ lib/sidekiq/health/queue_names.rb
@@ -1,6 +1,17 @@
+require 'yaml'
+
 module Sidekiq
   module Health
     class QueueNames
+      attr_reader :configuration
+
+      def initialize(configuration_file_path)
+        @configuration = YAML.load_file(configuration_file_path)
+      end
+
+      def get
+        configuration[:queues].map { |q| q.is_a?(Array) ? q[0] : q }
+      end
     end
   end
 end
--- spec/sidekiq/health/queue_names_spec.rb
+++ spec/sidekiq/health/queue_names_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe Sidekiq::Health::QueueNames do
-  context 'with configuration file available' do
+  let(:configuration_file_path) { 'spec/fixtures/config/sidekiq.yml' }
+
+  subject { described_class.new(configuration_file_path).get }
+
+  context 'when configuration file exists' do
     context 'with default and prioritized queue' do
-      it 'parses the configuration file'
+      it 'parses the configuration file' do
+        expect(subject).to eq ['default', 'myqueue']
+      end
     end
   end
 
-  context 'without configuration file available' do
-    it 'raises'
+  context 'when configuration file does not exist' do
+    let(:configuration_file_path) { 'non-existing-file.yml' }
+
+    it { expect { subject }.to raise_error Errno::ENOENT }
   end
 end
